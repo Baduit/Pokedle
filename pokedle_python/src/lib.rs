0c26b3d69e37b1c44c14d4ee7f84e7ceec5a1c9f
Lena Bertho <>
2023-11-01 22:38:28 +0100

    🎨Remove useless comment

--- pokedle_python/src/lib.rs
+++ pokedle_python/src/lib.rs
@@ -3,9 +3,6 @@ use pyo3::prelude::*;
 
 use pokedle_core::{GuessResult, Pokedle};
 
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
 #[pymodule]
 #[pyo3(name = "pokedle")]
 fn pokedle_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
