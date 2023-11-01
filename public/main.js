c8edc876d6bc7bbcb9e632da5958f6a8d89b2571
Lena Bertho <>
2023-11-02 00:09:06 +0100

    🎨Use await style and more consistency in using ;

--- public/main.js
+++ public/main.js
@@ -1,11 +1,11 @@
-let pokemon_names = []
+let pokemon_names = [];
 
 function onKeyPressed(event) {
-	const enterKeyCode = 13
+	const enterKeyCode = 13;
 
 
 	if (event.keyCode == enterKeyCode) {
-		try_guess_pokemon()
+		try_guess_pokemon();
 	}
 }
 
@@ -15,7 +15,7 @@ function is_local() {
 
 function add_row_in_result_table(results) {
 	if (results == null) {
-		results = [ "equal", "equal", "equal", "equal", "equal" ]
+		results = ["equal", "equal", "equal", "equal", "equal"];
 	}
 
 	let tbodyRef = document.getElementById('result_table').getElementsByTagName('tbody')[0];
@@ -36,74 +36,65 @@ function add_row_in_result_table(results) {
 	}
 }
 
-function try_guess_pokemon() {
-	let pokemon_name = document.getElementById("title_input").value
+async function try_guess_pokemon() {
+	let pokemon_name = document.getElementById("title_input").value;
 	// Capitalize first character
-	pokemon_name = pokemon_name.charAt(0).toUpperCase() + pokemon_name.slice(1)
+	pokemon_name = pokemon_name.charAt(0).toUpperCase() + pokemon_name.slice(1);
 	if (pokemon_name == "") {
-		return
+		return;
 	} else if (!pokemon_names.includes(pokemon_name)) {
-		alert('This pokemon does not exist.')
+		alert('This pokemon does not exist.');
 	}
 
 	const options = {
 		method: 'POST'
-	}
+	};
 
 	let url;
 	if (is_local()) {
-		url = new URL("http://localhost:3412/guess")
+		url = new URL("http://localhost:3412/guess");
 	} else {
-		url = new URL("https://pokedle.baduit.eu/guess")
+		url = new URL("https://pokedle.baduit.eu/guess");
 	}
 
-	url.searchParams.append("pokemon_name", pokemon_name)
-	url.searchParams.append("lang", "fr")
+	url.searchParams.append("pokemon_name", pokemon_name);
+	url.searchParams.append("lang", "fr");
 
-	fetch(url, options)
-		.then((response) => {
-			return response.text()
-		})
-		.then((text_response) => {
-			console.log(text_response)
-			let object_response = JSON.parse(text_response)
-			console.log(object_response)
-			add_row_in_result_table(object_response)
+	let response = await fetch(url, options);
+	let text_response = await response.text();
+	console.log(text_response);
+	let object_response = JSON.parse(text_response);
+	console.log(object_response);
 
-		})
+	add_row_in_result_table(object_response);
 }
 
 function update_pokemon_list() {
-	let element = document.getElementById("pokemon_list")
+	let element = document.getElementById("pokemon_list");
 	for (let i = 0; i < pokemon_names.length; ++i) {
-		let new_option = document.createElement("option")
-		new_option.value = pokemon_names[i]
-		element.appendChild(new_option)
+		let new_option = document.createElement("option");
+		new_option.value = pokemon_names[i];
+		element.appendChild(new_option);
 	}
 }
 
-function startup() {
+async function startup() {
 	const options = {
 		method: 'GET'
-	}
+	};
 
 	let pokemon_names_url;
 	if (is_local()) {
-		pokemon_names_url = new URL("http://localhost:3412/names")
+		pokemon_names_url = new URL("http://localhost:3412/names");
 	} else {
-		pokemon_names_url = new URL("https://pokedle.baduit.eu/names")
+		pokemon_names_url = new URL("https://pokedle.baduit.eu/names");
 	}
-	pokemon_names_url.searchParams.append('lang', 'fr')
-	fetch(pokemon_names_url, options)
-		.then((response) => {
-			return response.text()
-		})
-		.then((text_response) => {
-			console.log(text_response)
-			let object_response = JSON.parse(text_response)
-			pokemon_names = object_response
-			update_pokemon_list()
-		})
+	pokemon_names_url.searchParams.append('lang', 'fr');
+	let response = await fetch(pokemon_names_url, options);
+	let response_text = await response.text();
+	let object_response = JSON.parse(response_text);
+	pokemon_names = object_response;
+	update_pokemon_list();
 }
 
 startup()
